86b278f2be7021627ace4dd01a32bc1c5472f53d
Bayu Satiyo <itsyuukunz@gmail.com>
2026-06-12 02:14:19 +0700

    fix(PixelLab): Fix premium flags being overriden by HandlePurchases()
    Signed-off-by: Bayu Satiyo <itsyuukunz@gmail.com>

--- patches/src/main/kotlin/kiraio/lain/pixellab/premium/PremiumPatch.kt
+++ patches/src/main/kotlin/kiraio/lain/pixellab/premium/PremiumPatch.kt
@@ -1,8 +1,7 @@
 package kiraio.lain.pixellab.premium
 
-import app.morphe.patcher.extensions.InstructionExtensions.removeInstruction
+import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
 import app.morphe.patcher.patch.bytecodePatch
-import app.morphe.util.addInstructionsToEnd
 import kiraio.lain.pixellab.shared.Constants
 
 @Suppress("unused")
@@ -13,13 +12,21 @@ val premiumPatch = bytecodePatch(
 ) {
     compatibleWith(Constants.COMPATIBILITY)
     execute {
-        MainActivityOnCreateFingerprint.method.addInstructionsToEnd(
+        MainActivityHandlePurchasesFingerprint.method.addInstructions(
+            0,
             """
             const/4 v0, 0x1
+            
             invoke-virtual {p0, v0}, Lcom/imaginstudio/imagetools/pixellab/MainActivity;->makePremium(Z)V
+            
+            sget-object v1, Lcom/imaginstudio/imagetools/pixellab/MainActivity;->USER_PREMIUM:Ljava/lang/String;
+            
+            iget-object p1, p0, Lcom/imaginstudio/imagetools/pixellab/MainActivity;->tinydb:Lcom/imaginstudio/imagetools/pixellab/TinyDB;
+            
+            invoke-virtual {p1, v1, v0}, Lcom/imaginstudio/imagetools/pixellab/TinyDB;->putBoolean(Ljava/lang/String;Z)V
+            
+            return-void
             """.trimIndent()
         )
-        // Remove 'remove ads' button
-        MainActivityCreateMainMenuFingerprint.method.removeInstruction(MainActivityCreateMainMenuFingerprint.instructionMatches.first().index + 1)
     }
 }
